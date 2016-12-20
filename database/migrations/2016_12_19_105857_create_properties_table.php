d4972fd3eb63dfd970ac0231b75cff1711815c0b
Manikandan Arumugam <Manikandan.A@versatile-soft.com>
2016-12-20 12:28:10 +0530

    Property table seeding process & creation

---
+++ database/migrations/2016_12_19_105857_create_properties_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function(Blueprint $table)
+		{
+			$table->increments('id')->unsigned();
+            $table->string('name')->nullable();
+            $table->string('price')->nullable();
+            $table->string('bedrooms')->nullable();
+            $table->string('bathrooms')->nullable();
+            $table->string('storeys')->nullable();
+			$table->string('garages')->nullable();
+			$table->timestamps();
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('properties');
+    }
+}
